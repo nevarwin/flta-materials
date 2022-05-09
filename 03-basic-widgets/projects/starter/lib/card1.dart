7b369eead4a0cab6fd6ce57bc3947eb8cd5d1422
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-09 21:41:34 +0800

    decorate the container with image

--- 03-basic-widgets/projects/starter/lib/card1.dart
+++ 03-basic-widgets/projects/starter/lib/card1.dart
@@ -11,7 +11,24 @@ class Card1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(),
+      // TODO: Card1 Decorate Container
+      child: Container(
+        // TODO: Add a stack of text
+        padding: const EdgeInsets.all(16.0),
+        constraints: const BoxConstraints.expand(
+          height: 450,
+          width: 350,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag1.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+      ),
     );
   }
 }
