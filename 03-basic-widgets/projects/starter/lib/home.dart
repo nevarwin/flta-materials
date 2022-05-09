08ed347fb38f03a914cc833c6b284795d8fa86fa
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-09 20:21:04 +0800

    create home widget

---
+++ 03-basic-widgets/projects/starter/lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      // TODO: Show selected tab
+      body: Center(
+        child: Text(
+          'Let\'s get cooking 👩‍🍳',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+      ),
+      // TODO: Add bottom navigation bar
+    );
+  }
+}
