2bdf749da1256d31bc34144922580cb9b2cbaf60
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-11 21:43:22 +0800

    create explore screen

---
+++ 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatelessWidget {
+  final mockService = MockFooderlichService();
+
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO 1: Add TodayRecipeListView FutureBuilder
+    return const Center(
+      child: Text('Explore Screen'),
+    );
+  }
+}
