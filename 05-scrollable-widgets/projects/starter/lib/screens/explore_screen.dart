56c107a6ec3b06bb4b478ef8491f7527b8a7a57a
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-12 20:36:05 +0800

    nested 2 listviews

--- 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
+++ 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
@@ -17,10 +17,19 @@ class ExploreScreen extends StatelessWidget {
       builder: ((context, AsyncSnapshot<ExploreData> snapshot) {
         // TODO: Add Nested List Views
         if (snapshot.connectionState == ConnectionState.done) {
-          final recipes = snapshot.data?.todayRecipes ?? [];
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height: 16),
 
-          // TODO: Replace this with TodayRecipeListView
-          return TodayRecipeListView(recipes: recipes);
+              // TODO: Replace this with FriendPostListView
+              Container(
+                height: 400,
+                color: Colors.green,
+              ),
+            ],
+          );
         } else {
           return const Center(
             child: CircularProgressIndicator(),
