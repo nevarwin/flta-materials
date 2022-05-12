6594ff54094bff51c3e8f25f6d32bc8d022a3361
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-12 20:52:44 +0800

    added post list view

--- 05-scrollable-widgets/projects/starter/lib/components/friend_post_list_view.dart
+++ 05-scrollable-widgets/projects/starter/lib/components/friend_post_list_view.dart
@@ -6,11 +6,12 @@ import 'components.dart';
 import '../models/models.dart';
 
 class FriendPostListView extends StatelessWidget {
-  final List<Post> friendPosts;
   const FriendPostListView({
     Key? key,
     required this.friendPosts,
   }) : super(key: key);
+
+  final List<Post> friendPosts;
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +30,19 @@ class FriendPostListView extends StatelessWidget {
           ),
           const SizedBox(height: 16),
           // TODO: Add PostListView here
+          ListView.separated(
+              primary: false,
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                final post = friendPosts[index];
+                return FriendPostTile(post: post);
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 16);
+              },
+              itemCount: friendPosts.length),
           const SizedBox(height: 16),
         ],
       ),
