97cbc1276074e20f63e26827f4dfc92618de1b0f
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-12 21:06:20 +0800

    nested future widget to scaffold

--- 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
+++ 05-scrollable-widgets/projects/starter/lib/screens/explore_screen.dart
@@ -12,30 +12,30 @@ class ExploreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO 1: Add TodayRecipeListView FutureBuilder
-    return FutureBuilder(
-      future: mockService.getExploreData(),
-      builder: ((context, AsyncSnapshot<ExploreData> snapshot) {
-        // TODO: Add Nested List Views
-        if (snapshot.connectionState == ConnectionState.done) {
-          return ListView(
-            scrollDirection: Axis.vertical,
-            children: [
-              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
-              const SizedBox(height: 16),
+    return Scaffold(
+      body: FutureBuilder(
+        future: mockService.getExploreData(),
+        builder: ((context, AsyncSnapshot<ExploreData> snapshot) {
+          // TODO: Add Nested List Views
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+                const SizedBox(height: 16),
 
-              // TODO: Replace this with FriendPostListView
-              Container(
-                height: 400,
-                color: Colors.green,
-              ),
-            ],
-          );
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      }),
+                // TODO: Replace this with FriendPostListView
+                FriendPostListView(
+                    friendPosts: snapshot.data?.friendPosts ?? []),
+              ],
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }),
+      ),
     );
   }
 }
