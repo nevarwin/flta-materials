4ae9c23b26d3a7ae67679778d94aadf666f52ba0
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-12 20:36:14 +0800

    create friend post tile

---
+++ 05-scrollable-widgets/projects/starter/lib/components/friend_post_tile.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import '../components/components.dart';
+
+class FriendPostTile extends StatelessWidget {
+  final Post post;
+  const FriendPostTile({
+    Key? key,
+    required this.post,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CircleImage(
+          imageProvider: AssetImage(post.profileImageUrl),
+        ),
+        const SizedBox(height: 16.0),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(post.comment),
+              Text(
+                '$post.timestamp mins ago',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
