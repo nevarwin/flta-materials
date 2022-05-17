f55d14e0ecfb57a0a6f271340bc4cb5bfa6dfbff
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-17 20:25:39 +0800

    create recipe gridview

---
+++ 05-scrollable-widgets/projects/starter/lib/components/recipe_gridview.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import 'recipe_thumbnail.dart';
+
+class RecipeGridView extends StatelessWidget {
+  final List<SimpleRecipe> recipe;
+  const RecipeGridView({
+    Key? key,
+    required this.recipe,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 16,
+      ),
+      child: GridView.builder(
+        itemCount: recipe.length,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index) {
+          final simpleRecipe = recipe[index];
+          return RecipeThumbnail(recipe: simpleRecipe);
+        },
+      ),
+    );
+  }
+}
