8b2755da275187f2a36b0b66d8374abfc0952320
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-17 21:58:45 +0800

    added multiprovider to connect tabmanager

--- 06-interactive-widgets/projects/starter/lib/main.dart
+++ 06-interactive-widgets/projects/starter/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'fooderlich_theme.dart';
 import 'home.dart';
+import './models/models.dart';
 
 void main() {
   runApp(const Fooderlich());
@@ -17,7 +19,14 @@ class Fooderlich extends StatelessWidget {
       theme: theme,
       title: 'Fooderlich',
       // TODO 8: Replace this with MultiProvider
-      home: const Home(),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => TabManager(),
+          )
+        ],
+        child: const Home(),
+      ),
     );
   }
 }
