c5c6b9fd52d5f0dbe56f0b0b1268793534d89d7d
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-18 20:34:23 +0800

    create grocery item screen

---
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_item_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GroceryItemScreen extends StatelessWidget {
+  const GroceryItemScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
