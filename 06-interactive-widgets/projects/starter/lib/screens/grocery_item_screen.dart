e1d2f13e4283ec53b9296791238b21578000b0d2
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-18 21:01:26 +0800

    added properties

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_item_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_item_screen.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
 import '../models/grocery_item.dart';
 import '../models/models.dart';
 
@@ -26,6 +27,18 @@ class GroceryItemScreen extends StatefulWidget {
 
 class _GroceryItemScreenState extends State<GroceryItemScreen> {
   // TODO: Add grocery item screen state properties
+  final _nameController = TextEditingController();
+  final _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  var _currentSliderValue = 0;
+
+  // TODO: Add initState()
+
+// TODO: Add dispose()
+
   @override
   Widget build(BuildContext context) {
     // TODO 12: Add GroceryItemScreen Scaffold
