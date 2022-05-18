ce0d5bdb9017f325694ceef40427bf23f14e9535
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-05-18 20:40:00 +0800

    added constructors

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_item_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_item_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+import '../models/grocery_item.dart';
+import '../models/models.dart';
 
-class GroceryItemScreen extends StatelessWidget {
-  const GroceryItemScreen({Key? key}) : super(key: key);
+class GroceryItemScreen extends StatefulWidget {
+  final Function(GroceryItem) onCreate;
+  final Function(GroceryItem) onUpdate;
+  final GroceryItem? originalItem;
+  final bool isUpdating;
+
+  const GroceryItemScreen({
+    Key? key,
+    required this.onCreate,
+    required this.onUpdate,
+    this.originalItem,
+  })  : isUpdating = (originalItem != null),
+        super(key: key);
 
   @override
+  State<GroceryItemScreen> createState() => _GroceryItemScreenState();
+}
+
+class _GroceryItemScreenState extends State<GroceryItemScreen> {
+  // TODO: Add grocery item screen state properties
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    // TODO 12: Add GroceryItemScreen Scaffold
+    return Container(color: Colors.orange);
+    // TODO: Add buildNameField()
+
+    // TODO: Add buildImportanceField()
+
+    // TODO: ADD buildDateField()
+
+    // TODO: Add buildTimeField()
+
+    // TODO: Add buildColorPicker()
+
+    // TODO: Add buildQuantityField()
   }
 }
